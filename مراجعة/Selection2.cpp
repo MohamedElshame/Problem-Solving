b8bff063b55c115b8965cb2604887a737e8f6beb
Mohamed Elhshamy <mohamed20031023@gmail.com>
2025-04-27 14:33:35 +0300

    adding to morag3a

---
+++ مراجعة/Selection2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+// find maximum of 10 only using 2 variables
+int main() {
+    int a,temp;
+    temp=0;
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cin>>a;
+    if (a>temp) {
+        temp=a;
+    }
+    cout<<temp<<endl;
+}
+
+
